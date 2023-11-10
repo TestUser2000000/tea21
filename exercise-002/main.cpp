56937646dc6c35e933679a4a47a50146519192ff
TimoB200000 <149515706+TestUser2000000@users.noreply.github.com>
2023-11-10 10:43:32 +0100

    Fix typo in exercise-002

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -1,4 +1,4 @@
-#include <fmt/chrono.h>
+ #include <fmt/chrono.h>
 #include <fmt/format.h>
 
 #include "CLI/CLI.hpp"
@@ -6,6 +6,7 @@
 
 auto main(int argc, char **argv) -> int
 {
+    auto count = 20;
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -14,12 +15,18 @@ auto main(int argc, char **argv) -> int
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count",
+        count,
+        fmt::format("Create a vector with the given size default: {}", count));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
+    std::vector<int> data(count);
+    fmt::print("Created a vector with {} elements \n", count);
+    
 
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
@@ -32,3 +39,5 @@ auto main(int argc, char **argv) -> int
 
     return 0; /* exit gracefully*/
 }
+
+
